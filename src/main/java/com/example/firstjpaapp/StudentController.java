6ad02f8d80d8b060976b9cb45603929cb2740aba
sachin jadhao <jadhaosachin7171@gmail.com>
2023-07-09 14:05:36 +0530

    API Added

--- src/main/java/com/example/firstjpaapp/StudentController.java
+++ src/main/java/com/example/firstjpaapp/StudentController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -26,5 +28,42 @@ public class StudentController {
         }
 
         return new ResponseEntity(student,HttpStatus.FOUND);
+    }
+//    delete student by roll number
+    @DeleteMapping("/delete")
+    public ResponseEntity deleteStudent(@RequestParam("id") int rollNo){
+        String s = studentService.deleteStudent(rollNo);
+
+        return new ResponseEntity<>(s,HttpStatus.ACCEPTED);
+
+    }
+//    update the age of student based on roll number -> roll no and age as input
+    @PutMapping("/updateAge")
+    public ResponseEntity updateTheAge(@RequestParam("id") int rollNo, @RequestParam("age") int age){
+        Student student1 = studentService.updateTheAge(rollNo,age);
+        if(student1 != null)
+            return new ResponseEntity<>(student1,HttpStatus.OK);
+
+        return new ResponseEntity<>("student is not exists",HttpStatus.NOT_FOUND);
+    }
+
+//    find all the student who have age greater than 25
+    @GetMapping("/findAll")
+    public ResponseEntity findAll(){
+        List<Student> al =studentService.findAll();
+
+        return new ResponseEntity<>(al,HttpStatus.ACCEPTED);
+    }
+//    delete all the student
+//    find all the student with given name
+
+    @GetMapping("/findByName")
+    public ResponseEntity findStudentWithGivenName(@RequestParam("name") String name){
+        Student s1 = studentService.findStudentWithGivenName(name);
+
+        if(s1 != null){
+            return new ResponseEntity<>(s1,HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>("student not found",HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/firstjpaapp/StudentService.java
+++ src/main/java/com/example/firstjpaapp/StudentService.java
@@ -1,8 +1,10 @@
 package com.example.firstjpaapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,6 +24,48 @@ public class StudentService {
 
         if(student.isPresent()){
             return student.get();
+        }
+
+        return null;
+    }
+
+    public String deleteStudent(int rollNo) {
+        studentRepository.deleteById(rollNo);
+
+        return "Student Removed Successfully";
+    }
+
+    public List<Student> findAll() {
+        List<Student> al = studentRepository.findAll();
+
+        return al;
+
+    }
+
+
+    public Student updateTheAge(int rollNo, int age) {
+        Optional<Student> student1 = studentRepository.findById(rollNo);
+
+        if(student1.isPresent()){
+            student1.get().setAge(age);
+            studentRepository.save(student1.get());
+
+            return student1.get();
+        }
+
+        return null;
+
+
+
+    }
+
+    public Student findStudentWithGivenName(String name) {
+        List<Student> list = findAll();
+
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).getName() == name){
+                return list.get(i);
+            }
         }
 
         return null;
